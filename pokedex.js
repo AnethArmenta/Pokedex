baed5d51f57a2b5878425a0942c47425b58d2559
AnethArmenta <99218555+AnethArmenta@users.noreply.github.com>
2022-03-20 16:33:37 -0600

    add:files from cluod gui

---
+++ pokedex.js
@@ -0,0 +1,37 @@
+
+const pokenombre = document.getElementById('nombre');
+const pokeImg = document.getElementById("pokeImg");
+const poketipo = document.getElementById('poketipo');
+const pokehabil1 = document.getElementById('pokehabil')
+const pokehabil2 = document.getElementById('pokehabil2')
+const pokemov = document.getElementById('pokemov')
+const pokemov1 = document.getElementById('pokemov1')
+
+const fetchPokemon =() => { 
+const pokeNameInput = document.getElementById("pokeName");
+let pokeName = pokeNameInput.value;
+pokeName = pokeName.toLowerCase();
+const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+fetch(url).then((res)=> res.json())
+.then((data)=>{
+        //let id = ('00' + data.id).slice(-3);
+        pokenombre.innerHTML = data.name;
+        poketipo.innerHTML = data.types[0].type.name;
+        pokehabil1.innerHTML = data.abilities[0].ability.name;
+        pokehabil2.innerHTML = data.abilities[1].ability.name;
+        pokemov.innerHTML = data.moves[0].move.name;
+        pokemov1.innerHTML = data.moves[1].move.name;
+
+        let pokeimg = data.sprites.front_default;
+        pokeImage(pokeimg);
+    })
+    const pokeImage = (url) =>{
+        pokeImg.src = url;
+        
+
+}
+
+}
+
+
+
